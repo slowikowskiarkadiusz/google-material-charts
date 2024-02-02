51409963ae16b17b54a560c8a2355be014a817d7
Arkadiusz Słowikowski <slowikowskiarkadiusz@gmail.com>
2024-02-02 10:17:30 +0100

    removed js files

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const line_chart_1 = require("./charts/line/line.chart");
-new line_chart_1.LineChart(document.getElementById('diagram-container'), 'Line chart!');
-//# sourceMappingURL=index.js.map
