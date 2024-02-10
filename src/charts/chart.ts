11b0d7262ddd7f3f9c0e4e3125c9eaad1270d839
Arkadiusz Słowikowski <slowikowskiarkadiusz@gmail.com>
2024-02-10 12:35:32 +0100

    Added a bubble that appears on mouseover

--- src/charts/chart.ts
+++ src/charts/chart.ts
@@ -6,6 +6,7 @@ export abstract class Chart {
 
   protected svg: SVGElement;
   protected legend: HTMLDivElement;
+  protected bubble?: HTMLDivElement;
 
   protected constructor(protected parent: HTMLDivElement, title: string) {
     parent.classList.add(styles.chart);
@@ -17,6 +18,25 @@ export abstract class Chart {
 
     this.svg = document.createElementNS(Chart.svgNS, "svg");
     this.svg.classList.add(styles.chartContent);
+    this.svg.addEventListener('mouseover', (e: MouseEvent) => {
+      this.bubble?.remove();
+      this.bubble = this.parent.ownerDocument.createElement('div');
+      this.bubble.innerText = 'abc';
+      this.bubble.classList.add(styles.bubble);
+      this.parent.append(this.bubble);
+    });
+    this.svg.addEventListener('mousemove', (e: MouseEvent) => {
+      if (this.bubble) {
+        const bounds = this.bubble.getBoundingClientRect();
+        this.bubble!.style.top = `${ e.pageY - bounds.height - 10 }px`;
+        this.bubble!.style.left = `${ e.pageX - bounds.width / 2 }px`;
+      }
+    });
+    this.svg.addEventListener('mouseleave', (e: MouseEvent) => {
+      if (this.bubble) {
+        // this.bubble!.remove();
+      }
+    });
     parent.append(this.svg);
 
     this.legend = this.parent.ownerDocument.createElement('div');
